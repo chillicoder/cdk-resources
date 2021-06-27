ac06f42031f6f9733d1870f3563010577931a85a
Martin Trejo Chavez <chillicoder@gmail.com>
2021-06-26 21:00:27 -0500

    Setups a VPC and a bastion instance

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 
 from aws_cdk import core as cdk
+from aws_cdk.core import Tags
 
 # For consistency with TypeScript code, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -13,22 +14,9 @@ from cdk_resources.cdk_resources_stack import CdkResourcesStack
 
 
 app = core.App()
-CdkResourcesStack(app, "CdkResourcesStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+resources = CdkResourcesStack(app, "CdkResourcesStack", "192.168.0.0/21")
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+Tags.of(resources).add("project", "cdk-resources")
+Tags.of(resources).add("environment", "testing")
 
 app.synth()
--- cdk_resources/cdk_resources_stack.py
+++ cdk_resources/cdk_resources_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core as cdk
+from aws_cdk import aws_ec2 as ec2
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -9,7 +10,31 @@ from aws_cdk import core
 
 class CdkResourcesStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, cidr: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = ec2.Vpc(
+            self, "ThisVpc",
+            cidr=cidr,
+            max_azs=3,
+            subnet_configuration=[{
+                'subnetType': ec2.SubnetType.PUBLIC,
+                'name': 'Public',
+                'cidr_mask': 25
+            },{
+                'subnetType': ec2.SubnetType.PRIVATE,
+                'name': 'Private',
+                'cidr_mask': 24
+            },{
+                'subnetType': ec2.SubnetType.ISOLATED,
+                'name': 'Isolated',
+                'cidr_mask': 26
+            }]
+        )
+
+        bastion = ec2.BastionHostLinux(
+            self, "ThisBastionHost",
+            vpc = vpc,
+            instance_name="bastion",
+        )
